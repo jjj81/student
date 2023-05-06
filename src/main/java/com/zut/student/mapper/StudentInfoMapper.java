bb8d44c0c9b0cf98ef7deb71fdf84c3cd0307a97
jsw <jsw48929@gmail.com>
2023-05-07 01:16:29 +0800

    new

---
+++ src/main/java/com/zut/student/mapper/StudentInfoMapper.java
@@ -0,0 +1,9 @@
+
+package com.zut.student.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface StudentInfoMapper {
+}
+		
--- src/main/resources/sql/studentLoginInfo.sql
+++ src/main/resources/sql/studentLoginInfo.sql
@@ -5,6 +5,6 @@ drop table if exists studentLoginInfo;
 create table studentLoginInfo(
 studentId varchar(50) primary key,
 passWord varchar(500) not null,
-className varchar(30),
-inClass int default 0
+className varchar(30) not null,
+studentName varchar(30) 
 );
